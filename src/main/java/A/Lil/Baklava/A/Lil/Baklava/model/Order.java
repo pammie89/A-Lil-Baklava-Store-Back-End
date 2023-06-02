9e4ec1dbcae0e99414fb02c730818f3f42a1facc
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-02 10:44:15 -0500

    created Order model

---
+++ src/main/java/A/Lil/Baklava/A/Lil/Baklava/model/Order.java
@@ -0,0 +1,50 @@
+package A.Lil.Baklava.A.Lil.Baklava.model;
+
+public class Order {
+    private int orderId;
+    private int userId;
+    private int productId;
+    private int quantity;
+
+    public Order() {
+    }
+
+    public Order(int orderId, int userId, int productId, int quantity) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
