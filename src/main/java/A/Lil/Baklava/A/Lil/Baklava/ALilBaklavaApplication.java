a086a276e39a8a387c636cfae702ab28c798ecb0
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-02 10:34:20 -0500

    My first backend commit

---
+++ src/main/java/A/Lil/Baklava/A/Lil/Baklava/ALilBaklavaApplication.java
@@ -0,0 +1,13 @@
+package A.Lil.Baklava.A.Lil.Baklava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALilBaklavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALilBaklavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/A/Lil/Baklava/A/Lil/Baklava/ALilBaklavaApplicationTests.java
@@ -0,0 +1,13 @@
+package A.Lil.Baklava.A.Lil.Baklava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALilBaklavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
