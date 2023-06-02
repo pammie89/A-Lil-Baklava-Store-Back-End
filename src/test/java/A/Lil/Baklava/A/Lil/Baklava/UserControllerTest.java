310b866066192aed0e15f7e6e3492bba721b50ee
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-02 11:19:39 -0500

    wrote first failed test for UserController

---
+++ src/test/java/A/Lil/Baklava/A/Lil/Baklava/UserControllerTest.java
@@ -0,0 +1,43 @@
+package A.Lil.Baklava.A.Lil.Baklava;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+
+
+public class UserControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    private UserService userService;
+    private UserController userController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        userController = new UserController(userService);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    public void testGetUser_Success() throws Exception {
+        int userId = 1;
+        User user = new User(userId, "John", "john@example.com", "password");
+
+        when(userService.getUserById(userId)).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}", userId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(userId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("John"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("john@example.com"));
+    }
+}
