11e0c79112964b0bfc48d7aadacf08d36d56b52b
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-05 09:45:28 -0500

    GetAllOrders test passes

--- src/main/java/A/Lil/Baklava/A/Lil/Baklava/service/OrderService.java
+++ src/main/java/A/Lil/Baklava/A/Lil/Baklava/service/OrderService.java
@@ -1,8 +1,28 @@
 package A.Lil.Baklava.A.Lil.Baklava.service;
 
+import A.Lil.Baklava.A.Lil.Baklava.model.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderService {
+
+    private final List<Order> orders = new ArrayList<>();
+
+    public List<Order> getAllOrders() {
+        return orders;
+    }
+
+    public Order getOrderById(int orderId) {
+        for (Order order : orders) {
+            if (order.getOrderId() == orderId) {
+                return order;
+            }
+        }
+        return null;
+    }
+
 
 }
