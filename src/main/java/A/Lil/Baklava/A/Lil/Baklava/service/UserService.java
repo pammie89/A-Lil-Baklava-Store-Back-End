0557ddc088d9f573c6910b30f7400ec35b620095
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-06 15:40:19 -0500

    having to refactor user service, repositiory as it wasn't working after seed file.

--- src/main/java/A/Lil/Baklava/A/Lil/Baklava/service/UserService.java
+++ src/main/java/A/Lil/Baklava/A/Lil/Baklava/service/UserService.java
@@ -1,30 +1,34 @@
 package A.Lil.Baklava.A.Lil.Baklava.service;
 
 import A.Lil.Baklava.A.Lil.Baklava.model.User;
+import A.Lil.Baklava.A.Lil.Baklava.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class UserService {
-    private final Map<Integer, User> users;
+    private UserRepository userRepository;
 
-    public UserService() {
-        this.users = new HashMap<>();
-    }
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {this.userRepository = userRepository;}
 
-    public User getUserById(int userId) {
-        return users.get(userId);
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public User createUser(User user) {
         int userId = generateUserId();
         user.setUserId(userId);
-        users.put(userId, user);
+        user.put(userId, user);
         return user;
+    }
+
+    public User getUserById(int userId) {
+        return users.get(userId);
     }
 
     public List<User> getAllUsers() {
