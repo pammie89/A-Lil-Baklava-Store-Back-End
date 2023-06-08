217469ba0146f2b271f07b85ad501957946aee19
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-08 10:36:32 -0500

    feat(order): fix create order table error

--- src/main/java/A/Lil/Baklava/A/Lil/Baklava/model/Order.java
+++ src/main/java/A/Lil/Baklava/A/Lil/Baklava/model/Order.java
@@ -3,21 +3,53 @@ package A.Lil.Baklava.A.Lil.Baklava.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    @Column
-//    private int userId;
-//
-//    @Column
-//    private int productId;
+
+    @Column
+    private int productId;
 
     @Column
     private int quantity;
 
+    @Column
+    private int userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
 
 //    @ManyToMany
@@ -27,7 +59,5 @@ public class Order {
 //            inverseJoinColumns = @JoinColumn(name = "product_id")
 //    )
 //    private List<Product> products = new ArrayList<>();
-
-
 
 }
