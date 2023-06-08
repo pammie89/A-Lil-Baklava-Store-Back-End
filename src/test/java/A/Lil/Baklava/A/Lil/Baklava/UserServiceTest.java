181a225167b6b53433582618c3f171c8db444a76
Pam Afaneh <pamela.afaneh@gmail.com>
2023-06-08 17:34:49 -0500

    About to spin up a new branch to explore user model

--- src/test/java/A/Lil/Baklava/A/Lil/Baklava/UserServiceTest.java
+++ src/test/java/A/Lil/Baklava/A/Lil/Baklava/UserServiceTest.java
@@ -1,68 +1,69 @@
-package A.Lil.Baklava.A.Lil.Baklava;
-
-import A.Lil.Baklava.A.Lil.Baklava.controller.UserController;
-import A.Lil.Baklava.A.Lil.Baklava.model.User;
-import A.Lil.Baklava.A.Lil.Baklava.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-public class UserServiceTest {
+//package A.Lil.Baklava.A.Lil.Baklava;
 //
-//    private MockMvc mockMvc;
+//import A.Lil.Baklava.A.Lil.Baklava.controller.UserController;
+//import A.Lil.Baklava.A.Lil.Baklava.model.User;
+//import A.Lil.Baklava.A.Lil.Baklava.service.UserService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 //
-//    @Mock
-//    private UserService userService;
+//import java.util.Arrays;
+//import java.util.List;
 //
-//    @InjectMocks
-//    private UserController userController;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 //
-//    private ObjectMapper objectMapper;
-//
-//    @BeforeEach
-//    public void setup() {
-//        objectMapper = new ObjectMapper();
-//        MockitoAnnotations.openMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-//    }
-//
-//    @Test
-//    public void testGetUserById() throws Exception {
-//        int userId = 1;
-//        User user = new User("John Doe", "john@example.com", "password");
-//
-//        when(userService.getUserById(userId)).thenReturn(user);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}", userId))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.userId").value(userId))
-//                .andExpect(jsonPath("$.name").value("John Doe"))
-//                .andExpect(jsonPath("$.email").value("john@example.com"))
-//                .andExpect(jsonPath("$.password").value("password"))
-//                .andDo(print());
-//
-//        verify(userService, times(1)).getUserById(userId);
-//    }
-//
+//public class UserServiceTest {
+////
+////    private MockMvc mockMvc;
+////
+////    @Mock
+////    private UserService userService;
+////
+////    @InjectMocks
+////    private UserController userController;
+////
+////    private ObjectMapper objectMapper;
+////
+////    @BeforeEach
+////    public void setup() {
+////        objectMapper = new ObjectMapper();
+////        MockitoAnnotations.openMocks(this);
+////        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+////    }
+////
+////    @Test
+////    public void testGetUserById() throws Exception {
+////        int userId = 1;
+////        User user = new User("John Doe", "john@example.com", "password");
+////
+////        when(userService.getUserById(userId)).thenReturn(user);
+////
+////        mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}", userId))
+////                .andExpect(status().isOk())
+////                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+////                .andExpect(jsonPath("$.userId").value(userId))
+////                .andExpect(jsonPath("$.name").value("John Doe"))
+////                .andExpect(jsonPath("$.email").value("john@example.com"))
+////                .andExpect(jsonPath("$.password").value("password"))
+////                .andDo(print());
+////
+////        verify(userService, times(1)).getUserById(userId);
+////    }
+////
 //    @Test
 //    public void testCreateUser() throws Exception {
 //        User user = new User( "John Doe", "john@example.com", "password");
+//        ObjectMapper objectMapper = null;
 //        String userJson = objectMapper.writeValueAsString(user);
 //
 //        when(userService.createUser(any(User.class))).thenReturn(user);
@@ -81,29 +82,29 @@ public class UserServiceTest {
 //        verify(userService, times(1)).createUser(any(User.class));
 //    }
 //
-//    @Test
-//    public void testGetAllUsers() throws Exception {
-//        User user1 = new User( "John Doe", "john@example.com", "password");
-//        User user2 = new User("Jane Smith", "jane@example.com", "password");
-//        List<User> users = Arrays.asList(user1, user2);
-//
-//        when(userService.getAllUsers()).thenReturn(users);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$[0].userId").value(1))
-//                .andExpect(jsonPath("$[0].name").value("John Doe"))
-//                .andExpect(jsonPath("$[0].email").value("john@example.com"))
-//                .andExpect(jsonPath("$[0].password").value("password"))
-//                .andExpect(jsonPath("$[1].userId").value(2))
-//                .andExpect(jsonPath("$[1].name").value("Jane Smith"))
-//                .andExpect(jsonPath("$[1].email").value("jane@example.com"))
-//                .andExpect(jsonPath("$[1].password").value("password"))
-//                .andDo(print());
-//        verify(userService, times(1)).getAllUsers();
-//    }
-
-
-
-}
+////    @Test
+////    public void testGetAllUsers() throws Exception {
+////        User user1 = new User( "John Doe", "john@example.com", "password");
+////        User user2 = new User("Jane Smith", "jane@example.com", "password");
+////        List<User> users = Arrays.asList(user1, user2);
+////
+////        when(userService.getAllUsers()).thenReturn(users);
+////
+////        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+////                .andExpect(status().isOk())
+////                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+////                .andExpect(jsonPath("$[0].userId").value(1))
+////                .andExpect(jsonPath("$[0].name").value("John Doe"))
+////                .andExpect(jsonPath("$[0].email").value("john@example.com"))
+////                .andExpect(jsonPath("$[0].password").value("password"))
+////                .andExpect(jsonPath("$[1].userId").value(2))
+////                .andExpect(jsonPath("$[1].name").value("Jane Smith"))
+////                .andExpect(jsonPath("$[1].email").value("jane@example.com"))
+////                .andExpect(jsonPath("$[1].password").value("password"))
+////                .andDo(print());
+////        verify(userService, times(1)).getAllUsers();
+////    }
+////
+////
+////
+//}
